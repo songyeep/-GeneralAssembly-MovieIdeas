3bdd8e706ad79f998337ac6abaf5ceed81b4fa66
songyeep <songyeep@gmail.com>
2015-01-18 23:31:34 +0800

    added type effect

--- app/models/movie_idea.rb
+++ app/models/movie_idea.rb
@@ -2,7 +2,6 @@ class MovieIdea < ActiveRecord::Base
 
   belongs_to :user
   acts_as_votable
-  acts_as_commentable
 
   validates_presence_of :synopsis, :more_info, :user_id
   validates_uniqueness_of :synopsis, :more_info
